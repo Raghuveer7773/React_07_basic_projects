17f93ae40ca01ec12ffd15ecc8d47bb418d59a5d
Raghuvver <raghuveersinghrathore245@gmail.com>
2026-05-19 13:07:36 +0530

    Initial commit

---
+++ project_01_brand_page/src/components/Navigation.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+        <nav>
+        <div className='logo'>
+          <img src="/images/brand_logo.png" alt="" />
+        </div>
+        <ul>
+          <li href="#">Home</li>
+          <li href="#">Location</li>
+          <li href="#">About</li>
+          <li href="#">Contact</li>
+        </ul>
+
+        <button>Login</button>
+      </nav>
+    </div>
+  )
+}
+
+export default Navigation
